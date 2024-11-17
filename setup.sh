1e26b14dffb7b81b67c9773e3677291d2cff3ae6
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-11-17 10:33:20 -0500

    improve setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,14 @@ sudo apt update
 sudo apt install tmux -y
 
 tmux -V
+
+sudo apt update
+sudo apt install nodejs npm -y
+
+sudo npm install -g yarn
+
+yarn --version
+
+yarn install
+
+tmux new-session -d -s ai3-api 'yarn start' \; setw -g mouse on
